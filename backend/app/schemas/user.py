35a18f201990d977dfdb0306e621f0016ff20488
kdamiiii <kurtdamian020199@gmail.com>
2025-11-12 04:11:56 +0800

    Added update and delete for users

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,7 +1,13 @@
+from typing import Optional
 from pydantic import BaseModel, EmailStr
+
+from app.schemas.session import SessionDetailsOut
 
 class UserBase(BaseModel):
     email: EmailStr
+    first_name:str
+    middle_name:Optional[str]= None
+    last_name:str
     password: str
 
 class UserCreate(UserBase):
@@ -11,3 +17,13 @@ class UserOut(UserBase):
     id: int
 
     model_config = {"from_attributes": True}
+
+class UserDetailsOut(UserOut):
+    sessions: list[SessionDetailsOut] = []
+
+class UserUpdate(BaseModel):
+    email: Optional[EmailStr] = None
+    first_name: Optional[str] = None
+    middle_name: Optional[str] = None
+    last_name: Optional[str] = None
+    password: Optional[str] = None
